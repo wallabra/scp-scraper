69ad7300ea84a08b2c9f4cadf7fd7d9acaf984b6
Gustavo6046 <rehermann6046@gmail.com>
2020-06-14 16:45:04 -0300

    Some refactoring, also fixed README

--- README.md
+++ README.md
@@ -10,4 +10,22 @@ the website, and `cheerio` jQuery to parse this data.
 
 ```js
 > const scraper = require('scp-scrape');
+> let entry; scraper('http://www.scp-wiki.net/scp-002').then((res) => { entry = res; console.log('done'); });
+Promise { <pending> }
+done
+> scraper.print(entry)
+|-----------------------------|
+Item #:        SCP-002
+Object Class:  Euclid
+ -
+SCP-002 is to remain connected to a suitable power supply at all times, to keep it in what appears
+to be a recharging mode. In case of electrical outage, the emergency barrier between the object and
+the facility is to be closed and the immediate area evacuated. Once facility power is
+re-established, alternating bursts of X-ray and ultraviolet light must strobe the area until
+SCP-002 is re-affixed to the power supply and returned to recharging mode. Containment area is to
+be kept at negative air pressure at all times.
+
+  [...]
+
+|-----------------------------|
 ```
--- index.js
+++ index.js
@@ -41,5 +41,107 @@ let scrapeScp = module.exports = function scrapeScp(url) {
     });
 }
 
+function doCaps(bigText, maxLen) {
+    let lines = [];
+    let outLine = [];
+    
+    // process each input line separately, to
+    // preserve existing line splits
+    bigText.split('\n').forEach((aLine, aLineInd) => {
+        let affordable = maxLen; // character space still affordable
+        
+        if (aLineInd > 0) {
+            // make the existing newlines twofold
+            lines.push('');
+        }
+        
+        aLine.split(' ').forEach((aWord) => {
+            // 1 if outLine is non-empty, else 0
+            // (used to account for leading spaces before all but the first word)
+            let leadSpace = +(outLine.length != 0);
+
+            // previewed new value for 'affordable'
+            let newAfford = affordable - leadSpace - aWord.length;
+        
+            while (newAfford <= 0) {
+                // this word would not fit in the current
+                // line, so break it
+                
+                if (!outLine.length) {
+                    // if affordable is still <= 0 with just
+                    // this one word, then the word is nonetheless
+                    // bigger than a single line; too large for
+                    // soft word wrapping.
+                    // (bring in the scissors!)
+
+                    let splitInd = maxLen;
+                    let hyphen = false;
+                    
+                    if (splitInd > 1) {
+                        // if the split position is OK with it, account for
+                        // a hyphen width too when splicing the word
+                        splitInd -= 1;
+                        hyphen = true;
+                    }
+                
+                    // push this splice of the word (the one that fits) in    
+                    outLine.push(aWord.slice(0, splitInd) + (hyphen ? '-' : ''));
+                    aWord = aWord.slice(splitInd);
+                }
+                
+                lines.push(outLine.join(' '));
+                newAfford = maxLen - aWord.length;
+                outLine = [];
+            }
+            
+            outLine.push(aWord);
+            affordable = newAfford;
+        });
+
+        if (outLine.length) { // the last line is non-empty
+            lines.push(outLine.join(' '));
+            outLine = [];
+        }
+    });
+    
+    // sanity checks
+    lines.forEach((resLine) => {
+        if (resLine.length > maxLen) {
+            console.error(lines);
+            throw new Error(`Soft wrapping algorithm failed to split line (${resLine.length} > ${maxLen}) - ` + JSON.stringify(resLine));
+        }
+    });
+    
+    return lines;
+}
+
+function* formatEntry(entry, procLineLen) {
+    yield "|-----------------------------|";
+    
+    yield `Item #:        ${entry.index}`;
+    yield `Object Class:  ${entry.class}`;
+    
+    yield " -";
+    
+    // keep in mind that HTML formatting
+    // (e.g. <b>highlighted</b>) will be
+    // stripped, because of how scrapeFrom
+    // works.
+
+    let text = entry.procedures.join('\n');
+    
+    // soft-cap each line, for extra
+    // terminal goodness :D
+    yield* doCaps(text, procLineLen || 100);
+
+    yield "|-----------------------------|";
+}
+
+function printEntry(entry, procLineLen) {
+    for (let value of formatEntry(entry, procLineLen)) console.log(value);
+}
+
 scrapeScp.load = scrapeFrom;
+scrapeScp.format = formatEntry;
+scrapeScp.print = printEntry;
 
